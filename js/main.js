1e3827c1bb15289169e787e0a1a6e209afe927e9
DaemonLibra <40869874+daemonLibra@users.noreply.github.com>
2019-07-15 20:18:21 +0200

    update to js and html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+
+function loadTitels() {
+    document.getElementById('title_head').innerHTML ="Meine Bewerbung für die Universität Karlstad";
+
+    document.getElementById('title_It').innerHTML ="Informatik";
+
+    document.getElementById('title_Wi').innerHTML ="Wirtschaft";
+
+    document.getElementById('title_Int').innerHTML="Warum möchte ich an der Universität Karlstad studieren?";
+
+    document.getElementById('map_title').innerHTML="Karlstad auf einer Karte mit der Leaflet Api";
+}
+
+function loadLeads() {
+    document.getElementById('lead_It').innerHTML ="Informatik beeinflusst mein Denken. Ich sehe nicht mehr ein Spiel als nur ein Spiel, vielmehr überlege ich mir, wie es programmiert wurde und was man daran verbessern könnte. Ich versuche nicht technische Probleme einfach hinzunehmen, sondern bemühe mich das Problem zu lösen und es vorzubeugen.\n" +
+        "\n" +
+        "            Mein allgemeines Interesse an Computern wurde schon in jungen Jahren geweckt als mein Vater mir und meinen Bruder jeweils einen Computer zusammenbaute mit Windows 98 und 2000, unseren Geburtsjahren.\n" +
+        "\n" +
+        "            Über die Jahre hinweg hat er mir viel über Computer beigebracht und wir haben schon viele Tage und Nächte mit dem Lösen von technischen Problemen verbracht. ";
+
+    document.getElementById('lead_Wi').innerHTML ="Früher arbeitete ich in der Kanzlei meiner Mutter, welche Steuerberaterin und Dozentin für Steuerrecht ist, und ich half als Aushilfe bei der Buchhaltung sowie der Bearbeitung von Skripten und Präsentationen. Diese 2-Jährigen Erfahrungen und die Vorlesungen bei der DHBW haben bei mir auch ein Interesse an wirtschaftlichen Themen geweckt.";
+
+
+    document.getElementById('lead_Int').innerHTML="        Als ich hörte das die DHBW eine Partner Uni in Karlstad hat erinnerte ich mich gerne zurück, dass ich mit der Familie in Mariebergsskogen gespielt habe und wir Mårbacka besuchten.\n" +
+        "        Da ich zur Hälfte Schwede bin und täglich mit meiner Mutter schwedisch sprechen, wusste ich, dass ich mir die Chance nicht entgehen lassen sollte mich für den Auslandsaufenthalt zu\n" +
+        "        bewerben.\n" +
+        "        Ich denke das Schweden (Värmland) die ideale Möglichkeit ist, um meine Ziel zu erreichen. Warum? Weil es zweifellos ein globaler Hotspot für Technologie ist und ich bei dem Weltmarktführer Wolters kluwer Software für Steuerberater programmiere. Zusätzlich darf ich meine Schwedischkenntnisse verbessern und kann damit im Konzern wolters kluwer die deutsch- schwedisch Kooperation unterstützen.\n";
+
+}
+
+loadTitels();
+loadLeads();
+
 const urlImg_1 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/MariebergsSkogen50.JPG/1920px-MariebergsSkogen50.JPG';
 const urlImg_2 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Selma_Lagerlofs_Marbacka_at_Sunne.jpg/1920px-Selma_Lagerlofs_Marbacka_at_Sunne.jpg';
 const mymap = L.map('mapid').setView([59.3793, 13.50357],13);
@@ -9,6 +42,8 @@ const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl, {att});
 
 tiles.addTo(mymap);
+
+L.marker([59.3793,13.50357]).addTo(mymap);
 
 async function GetImgKarlstad()
 {
@@ -22,5 +57,5 @@ async function GetImgKarlstad()
 
 GetImgKarlstad();
 
-L.marker([59.3793,13.50357]).addTo(mymap);
+
 
